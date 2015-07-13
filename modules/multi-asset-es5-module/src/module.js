5f3cf354053988eb4a729fbad5fe2630df056675
dchambers <dominic.chambers@gmail.com>
2015-07-13 05:26:00 +0100

    Begin conversion of modules to use JSPM.

--- modules/multi-asset-es5-module/src/module.js
+++ modules/multi-asset-es5-module/src/module.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('./dep.js');
-require('./style.css');
+require('./style.css!');
 
 module.exports = function() {
 	return '<span class="multi-asset-es5-module">@Multi-Asset ES5 Module</span>';
--- modules/multi-asset-es5-module/test/some-test.js
+++ modules/multi-asset-es5-module/test/some-test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var chai = require('chai');
 var expect = chai.expect;
 
