07e33b89d69f6488037d3ed2fcdcb1f740ecdacb
dchambers <dominic.chambers@gmail.com>
2015-08-06 23:04:00 +0100

    Fix broken require paths

--- apps/npm-app/src/app.js
+++ apps/npm-app/src/app.js
@@ -1,5 +1,5 @@
-import es5Module from 'es5-module';
-import es6Module from 'es6-module';
+import es5Module from '@dchambers/es5-module';
+import es6Module from '@dchambers/es6-module';
 
 document.body.innerHTML = `
 	<div id="es5-module">${es5Module()}</div>
