766f66ed91aa83a5e486775c7cd4cd34440739cd
dchambers <dominic.chambers@gmail.com>
2015-10-12 15:57:44 +0100

    Don't load unnecessary source code in Karma

--- modules/es5-module/karma.conf.js
+++ modules/es5-module/karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'src/**/*.js',
 			'test/**/*.js'
 		],
 
@@ -24,7 +23,6 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'src/**/*.js': ['browserify'],
 			'test/**/*.js': ['browserify']
 		},
 
